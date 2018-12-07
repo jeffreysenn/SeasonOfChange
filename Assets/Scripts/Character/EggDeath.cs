507b2fb075e361c06a3dd4b4148a11ead00009f3
Marcus_Ford <mackanford@hotmail.com>
2018-12-07 22:59:08 +0100

    Beggining of the Egg

---
+++ Assets/Scripts/Character/EggDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggDeath : MonoBehaviour {
+
+	void EggCollision(Collision col)
+	{
+		if (col.gameObject.name == "DeathTerrain")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
