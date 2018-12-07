d35e8b7a08c4c7e59f2b9786774fc28c598e1da3
Caroken <axelthomsen@hotmail.com>
2018-12-08 00:49:11 +0100

    MovementDash

---
+++ Assets/Source/Character/MovementDash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDash : MonoBehaviour
+{
+    public float dashVelocity = 50;
+    public const float maxDashTime = 1.0f;
+    public float dashStoppingSpeed = 0.1f;
+    float currentDashTime = 0;
+    public void Dash()
+    {
+        if (Input.GetKey(KeyCode.Y) == true)
+        {
+            while (currentDashTime <= dashStoppingSpeed)
+            {
+                GetComponent<Rigidbody>().velocity = transform.forward * dashVelocity;
+                currentDashTime = dashStoppingSpeed * Time.deltaTime;
+            }
+        }
+    }
+}
